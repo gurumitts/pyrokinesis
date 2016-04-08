beb1256b460a1da2be0aae775b4a71937f29ab64
gurumitts <miteshcoder1001@gmail.com>
2016-04-07 22:14:00 -0500

    more modifications

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo stopping control
-sudo kill -9 `ps aux | grep control.py | awk '{print $2}'`
 
-
-echo stopping web frontend
-sudo kill -9 `ps aux | grep web.py | awk '{print $2}'`
+echo stopping
+kill -9 `ps aux | grep run.py | awk '{print $2}'`
 
